b2f417b872990e7a614c530f5884064baf5d179c
JBLka <123543658+JBLka@users.noreply.github.com>
2024-02-25 19:51:11 +0300

    jkh

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
@@ -13,4 +12,3 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
