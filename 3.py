1b30f79aa0231579c346698d8c2b4a26b819bd85
JBLka <123543658+JBLka@users.noreply.github.com>
2024-02-26 20:22:07 +0300

    Задание 3

--- 3.py
+++ 3.py
@@ -1,19 +1,19 @@
 import csv
 
-with open('students.csv') as file:
+with open('students.csv', encoding='utf-8') as file:
     reader = csv.DictReader(file, delimiter=',')
     reader = list(reader)
     request = input()
-    while request != 'СТОП':
+    while request != 'СТОП':   # Ожидаем остановку
         is_real_project = False
         for i in range(len(reader)):
-            if reader[i]['titleProject_id'] == request:
+            if reader[i]['titleProject_id'] == request:    # Если введеный номер равен номеру проекта
                 project_id = reader[i]['titleProject_id']
                 name = reader[i]['Name'].split()
                 score = reader[i]['score']
                 print(f'Проект № {project_id} делал: {name[1][0]}. {name[0]} он(а) получил(а) оценку - {score}')
-                is_real_project = True
+                is_real_project = True   # Проект найден
                 break
-        if not is_real_project:
+        if not is_real_project:          # Если проект не найден
             print('Ничего не найдено')
         request = input()
