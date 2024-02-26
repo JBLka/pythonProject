7c62842ea1365c5b68f06bfe8e73d747bd81b733
JBLka <123543658+JBLka@users.noreply.github.com>
2024-02-26 20:22:18 +0300

    Задание 5

--- 5.py
+++ 5.py
@@ -1,9 +1,9 @@
 import csv
 
 
-def hash(s):
+def hash(s):  # Хеширование под данным из задачи
     p = 67
-    m = 10 ** 9 + 9  # это взяла из задания
+    m = 10 ** 9 + 9
     alph = ''.join([chr(i) for i in range(ord('А'), ord('я') + 1)])
     alph += 'ёЁ '
     hash_code = 0
@@ -18,8 +18,8 @@ students_with_hash = []
 with open('students.csv', encoding='utf-8') as f:
     reader = list(csv.DictReader(f, delimiter=','))
     for row in reader:
-        row['id'] = hash(row['Name'])
-        students_with_hash.append(ow)
+        row['id'] = hash(row['Name'])   # Меняем id на хешированное имя
+        students_with_hash.append(row)
 with open('student+_with_hash.csv', 'w', encoding='utf-8') as f:
     writer = csv.DictWriter(f, fieldnames=['id', 'Name', 'titleProject_id', 'class', 'score'])
     writer.writeheader()
